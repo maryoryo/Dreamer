d749f5d5ddc732644b41e5e74597b79e425c7e56
maryoryo <liyomaxxde@gmail.com>
2021-08-21 19:05:51 +0000

    [create]home

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,15 @@
 Rails.application.routes.draw do
+  
+  
+  scope module: :public do
+    devise_for :users, controllers: {
+        sessions: 'users/sessions',
+        passwords: 'users/passwords',
+        registrations: 'users/registrations'
+      }
+  end
+  
+  get 'homes/top'
+  get 'homes/index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
